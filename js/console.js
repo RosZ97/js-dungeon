eb984fbdf9ca7644f25277de45528adfe04d3b9c
RosZ97 <48953034+RosZ97@users.noreply.github.com>
2023-12-25 15:15:13 +0100

    Update console

--- js/console.js
+++ js/console.js
@@ -9,7 +9,7 @@ const std = {
             var args = Array.prototype.slice.call(arguments);
             for(var i=0;i<args.length;i++){
                 var node = std.createLogNode(args[i]);
-                this.dom.appendChild(node);
+                std.dom.appendChild(node);
             }
         };
 
